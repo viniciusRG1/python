48e28589515ffe8d6dda998362bf759b875d20ab
vinicius <viniciusxsantosx@hotmail.com.br>
2025-01-28 14:56:16 -0300

    study python

---
+++ main.py
@@ -0,0 +1,70 @@
+'''
+variavel_a = 'olá mundo'
+
+print(variavel_a)
+
+variavel_b = input('digite algo ')
+
+print(variavel_b)
+
+a = type(variavel_a)
+b = type(variavel_b)
+
+print(a, b)
+'''
+'''
+number1 = 5
+number2 = 5.
+number3 = number1+number2
+
+print(number3)
+
+number1 = number1 + number1 + 10
+
+print(number1)
+
+number1 += 3
+
+print(number1)
+
+number1 = 10**3
+
+print(number1)
+
+n_c = 5 + 1j
+
+n_c2 = n_c+n_c
+
+print(n_c2)
+
+strNumber = '123'
+
+numberfloat = float(strNumber)
+
+print(numberfloat)
+'''
+'''
+a = 'ola \n mundo \n'
+
+print(a)
+
+print(a + a)
+
+print(a*5)
+'''
+lista = [1,2,3,4]
+
+print(lista)
+
+lista += ['lapis'] # add palavras
+
+print(lista)
+
+lista += 'lapis' # add letra por letra
+
+print(lista)
+
+print(lista[0])
+
+print(len(lista)) # retorna o tamanho da lista
+
